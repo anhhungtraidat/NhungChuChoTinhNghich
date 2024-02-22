44457d5040ac57635c3269c92116293e8f90949d
anhhungtraidat <160729484+anhhungtraidat@users.noreply.github.com>
2024-02-22 10:57:22 +0000

    hello

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,9 @@
 package com.mycompany.app;
 
 public class App {
+    
     public static void main(String[] args) {
-        System.out.println("Hello Remote World!");
+        System.out.println("Hello Java");
+        System.out.println("Hello Java");
     }
 }
