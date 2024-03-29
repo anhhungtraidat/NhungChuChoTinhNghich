3a7cc426ced64349e20bb83d7d2a87ad4ecbd5b9
Truong DH Phenikaa <160729484+anhhungtraidat@users.noreply.github.com>
2024-03-29 16:39:22 +0000

    week2

---
+++ src/main/java/com/mycompany/app/week2/AccessControlE1/AccessControlExercise1.java
@@ -0,0 +1,10 @@
+package com.mycompany.app.week2.AccessControlE1;
+
+import com.mycompany.app.week2.MyClass;
+
+public class AccessControlExercise1 {
+    public static void accesscontrolexercise1 () {
+       
+    }
+    
+}
